0fe18135cf47ad27b2355212d38c24dddbcb33ed
Egor Ishutkin <EIIshutkin@1cbit.ru>
2024-05-16 23:24:25 +0300

    refactor
    Начата работа с модулем Yandex
    
    Co-authored-by: Egor Ishutkin

--- internal/repo/yandex/backup.go
+++ internal/repo/yandex/backup.go
@@ -1,1 +1,31 @@
 package yandex
+
+type BackupYandex struct {
+	Token string
+}
+
+func (y *BackupYandex) GetToken() string {
+	return y.Token
+}
+
+func (y *BackupYandex) SetToken(token string) {
+	y.Token = token
+}
+
+func NewBackupYandex(token string) *BackupYandex {
+	return &BackupYandex{
+		Token: token,
+	}
+}
+
+func (y *BackupYandex) createResource(resourcePath string) error {
+
+}
+
+func (y *BackupYandex) createLink(resourceName string) error {
+
+}
+
+func (y *BackupYandex) uploadFile(link string, path string) error {
+
+}
